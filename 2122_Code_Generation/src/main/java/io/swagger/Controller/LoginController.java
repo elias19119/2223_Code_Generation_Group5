40884f8b6adedc4fb16d8c3d931d1df14a35fc0b
Elias Al Khouri Hanna <54850558+elias19119@users.noreply.github.com>
2023-06-09 16:09:58 +0200

    Remove annotation

--- 2122_Code_Generation/src/main/java/io/swagger/Controller/LoginController.java
+++ 2122_Code_Generation/src/main/java/io/swagger/Controller/LoginController.java
@@ -19,7 +19,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 @Controller
 @RestController
